953188787f5174fb164e02dcf9d6ac3e8ca34ccf
bulkacity <112503952+bulkacity@users.noreply.github.com>
2023-01-12 16:39:08 -0500

    added the following product routes and testing in insomia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -71,6 +71,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ 
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -137,6 +138,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      rs.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
